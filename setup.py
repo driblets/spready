12bc3e7fe42f47ece7f15fd4cd70d0df99c8ea38
Muthupandian <muthupandianthangarajan@gmail.com>
2023-12-17 03:24:00 +0530

    updated long description

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ sys.path[0:0] = ['src/paravu']
 setup(
     name='spready',
     version='1.0',
-    description='CRUD application for Google Sheet',
+    description='Spready APP',
+    long_description='Spready distributed API',
     author='muthugit',
     author_email='base.muthupandian@gmail.com',
     url='https://muthupandian.in',
