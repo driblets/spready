ba6da58bc7564a731148f318fb80405b26ed4f9a
Muthupandian <muthupandianthangarajan@gmail.com>
2023-12-17 03:25:44 +0530

    updated version

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 import sys
-sys.path[0:0] = ['src/paravu']
+sys.path[0:0] = ['src/spready']
 
 
 setup(
     name='spready',
-    version='1.0',
+    version='1.1',
     description='Spready APP',
     long_description='Spready distributed API',
     author='muthugit',
@@ -13,5 +13,4 @@ setup(
     url='https://muthupandian.in',
     packages=(find_packages(where="src")),
     package_dir={"": "src"},
-
 )
