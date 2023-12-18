8747dd5c2c14b8f328e0d1f057cbee86cc8a14a6
Muthupandian <muthupandianthangarajan@gmail.com>
2023-12-18 11:25:51 +0530

    propert output

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ sys.path[0:0] = ['src/spready']
 
 setup(
     name='spready',
-    version='1.5',
+    version='1.6',
     description='Spready APP',
     long_description='Spready distributed API',
     author='muthugit',
--- src/spready/job.py
+++ src/spready/job.py
@@ -18,15 +18,19 @@ def runjob(routePath: str, params: Dict[str, Any], requestType: str):
     logger.info(f"Running function with {routePath} and {params}")
     if routePath in spreadyModules:
         function_string = spreadyModules[routePath]
-        print(f"Running function: {function_string}")
     else:
-        print(f"Route not found {routePath}")
-        raise ValueError("Route not found")
-    mod_name, func_name = function_string.rsplit('.',1)
-    logger.debug(f"Module name: {mod_name}")
-    logger.debug(f"Function name: {func_name}")
-    sys.path.append(os.getcwd())
-    mod = importlib.import_module(mod_name)
-    func = getattr(mod, func_name)
-    req = SPRequest(json=params, requestType=requestType)
-    return func(req)
+        logger.error(f"Route not found {routePath}")
+        return {"error": "Route not found"}
+    try:
+        mod_name, func_name = function_string.rsplit('.',1)
+        logger.debug(f"Module name: {mod_name}")
+        logger.debug(f"Function name: {func_name}")
+        sys.path.append(os.getcwd())
+        mod = importlib.import_module(mod_name)
+        func = getattr(mod, func_name)
+        req = SPRequest(json=params, requestType=requestType)
+        result = func(req)
+        logger.debug(f"Output: {result}")
+        return result
+    except Exception as _e:
+        return {"error": str(_e)}
